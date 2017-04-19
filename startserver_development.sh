db7956a18b6a5cd099463dd71ffc6ee00bb999dd
Maarten van Gompel <proycon@anaproy.nl>
2017-04-19 13:01:24 +0200

    was not adapted yet for this webservice

--- startserver_development.sh
+++ startserver_development.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z $PYTHONPATH ]; then
-    export PYTHONPATH=/vol/tensusers/eyilmaz/FAME/webservice/fame_align
+    export PYTHONPATH=/vol/tensusers/eyilmaz/FAME/webservice/oral_history
 else
-    export PYTHONPATH=/vol/tensusers/eyilmaz/FAME/webservice/fame_align:$PYTHONPATH
+    export PYTHONPATH=/vol/tensusers/eyilmaz/FAME/webservice/oral_history:$PYTHONPATH
 fi
-clamservice -d fame_align
+clamservice -d oral_history
