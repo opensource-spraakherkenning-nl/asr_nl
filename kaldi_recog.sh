474f9897d22be9ff2fcd3205d128f4215a37f767
Emre Yilmaz <emrey@kth.se>
2018-05-02 22:26:50 +0200

    ctm with speaker ids

--- kaldi_recog.sh
+++ kaldi_recog.sh
@@ -39,11 +39,11 @@ for inputfile in $inputdir/*; do
   ./scripts/ctm2xml.py $outdir $file_id $scratchdir
 
   # Create .rttm
-  spkr_seg=$target_dir/liumlog/$file_id.seg
-  cat $spkr_seg | sed -n '/;;/!p' | sort -nk3 | awk '{printf "SPEAKER %s %s %.2f %.2f <NA> <NA> %s <NA>\n", $1, $2, ($3 / 100), ($4 / 100), $8}' > $outdir/$file_id.rttm
+  spkr_seg=$target_dir/liumlog/${file_id}.seg
+  cat $spkr_seg | sed -n '/;;/!p' | sort -nk3 | awk '{printf "SPEAKER %s %s %.2f %.2f <NA> <NA> %s <NA>\n", $1, $2, ($3 / 100), ($4 / 100), $8}' > $outdir/${file_id}.rttm
 
   # Create .ctm with speaker ids
-  ./scripts/ctm2xml.py $outdir/$file_id.rttm $outdir/${file_id}.ctm
+  ./scripts/ctm2xml.py $outdir/${file_id}.rttm $outdir/${file_id}.ctm
 
   # Add sentence boundaries
   cat $outdir/${file_id}.ctm | perl scripts/wordpausestatistic.perl 1.0 $outdir/${file_id}.sent
