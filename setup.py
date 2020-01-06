216086a69fd4d2d3449722fb834fc9e497508abe
Maarten van Gompel <proycon@anaproy.nl>
2020-01-06 20:03:34 +0100

    forgot to actually add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+# -*- coding: utf8 -*-
+
+from __future__ import print_function
+
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname), 'r', encoding='utf-8').read()
+
+setup(
+    name = "oralhistory",
+    version = "0.2",
+    author = "Emre Yilmaz, Maarten van Gompel",
+    author_email = "",
+    description = ("Dutch Automatic Speech Recognition Webservice"),
+    license = "unknown",
+    keywords = "clam webservice rest nlp computational_linguistics rest",
+    url = "https://github.com/schemreier/oralhistory",
+    packages=['oralhistory'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Text Processing :: Linguistic",
+        "Programming Language :: Python :: 3"
+        "Operating System :: POSIX",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    package_data = {'oralhistory':['*.sh','*.wsgi','*.yml'] },
+    include_package_data=True,
+    install_requires=['CLAM >= 2.3']
+)
