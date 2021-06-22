a4774289093d5cce7d30c0a9892bab674fe0ed67
Maarten van Gompel <proycon@anaproy.nl>
2021-06-22 19:43:31 +0200

    final checkcheck fixes

--- oralhistory/kaldi_recog.sh
+++ oralhistory/kaldi_recog.sh
@@ -30,15 +30,15 @@ fatalerror() {
         cat "$target_dir/intermediate/log" >&2
         echo "[End output of intermediate log]">&2
         echo "[Output of intermediate lium logs]" >&2
-        cat $target_dir/intermediate/data/ALL/liumlog/*.log >&2
+        cat "$target_dir"/intermediate/data/ALL/liumlog/*.log >&2
         echo "[End output of intermediate log]">&2
         echo "[Output of other intermediate logs]" >&2
-        cat $target_dir/intermediate/data/ALL/log/*.log >&2
+        cat "$target_dir"/intermediate/data/ALL/log/*.log >&2
         echo "[End output of other intermediate log]">&2
         echo "[Output of kaldi decode logs]" >&2
-        cat $target_dir/intermediate/decode/log/decode*log >&2
+        cat "$target_dir"/intermediate/decode/log/decode*log >&2
         echo "[End of kaldi decode logs]" >&2
-        if [ ! -z "$debug" ]; then
+        if [ -n "$debug" ]; then
             echo "(cleaning intermediate files after error)">&2
             rm -Rf "$target_dir"
         fi
@@ -50,14 +50,14 @@ if [ -z "$topic" ]; then
     topic="OH"
 fi
 
-cd "$resourcedir"
-for inputfile in $inputdir/*; do
+cd "$resourcedir" || fatalerror "Resourcedir not found"
+for inputfile in "$inputdir"/*; do
   filename=$(basename "$inputfile")
   echo "Processing $filename" >&2
   extension="${filename##*.}"
   file_id=$(basename "$inputfile" ."$extension")
-  sox $inputfile -e signed-integer -c 1 -r 16000 -b 16 $scratchdir/${file_id}.wav || fatalerror "Failure calling sox"
-  target_dir=$scratchdir/${file_id}_$(date +"%y_%m_%d_%H_%M_%S_%N")
+  sox "$inputfile" -e signed-integer -c 1 -r 16000 -b 16 "$scratchdir/${file_id}.wav" || fatalerror "Failure calling sox"
+  target_dir="$scratchdir/${file_id}_$(date +"%y_%m_%d_%H_%M_%S_%N")"
   mkdir -p "$target_dir" || fatalerror "Unable to create temporary working directory $target_dir"
 
   if [[ "$topic" == "GN" ]]; then
